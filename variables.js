7bf8f87a12285a51b1b9adaaa0fbdc295c5257bd
Aryanupadhyay23 <au3162233@gmail.com>
2026-09-08 19:13:02 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,29 @@
+// var, let, const 
+// declaration and initialization
+
+var a;
+
+a = 12;
+console.log(a);
+
+a = 23;
+console.log(a);
+
+
+var a = 12;
+console.log(a);
+
+var a = 23;
+console.log(a);
+
+
+let b;
+
+b = 12;
+console.log(b);
+
+b = 34;
+console.log(b);
+
+const c = 45;
+console.log(a);
