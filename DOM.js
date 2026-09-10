9169429d76a33cd940b56cc49110dc22c2438ea2
Aryanupadhyay23 <au3162233@gmail.com>
2026-09-10 13:03:13 +0530

    DOM

---
+++ DOM.js
@@ -0,0 +1,41 @@
+// let h1 = document.querySelector("h1");
+// h1.innerText = "Hello harsh how are you"
+// h1.textContent = "Hello Aryan how are you"
+// h1.innerHTML = "<i>Hello rahul how are you</i>"
+// let abcd2 = document.querySelectorAll  ("h1");
+// console.dir(h1);
+
+// let a = document.querySelector("a");
+// console.log(a.removeAttribute("href"));
+// console.log(a.getAttribute("href"));
+// a.setAttribute("href","https://www.aryanupadhyay.com")
+// a.setAttribute("src","https://media.istockphoto.com/id/1420676204/photo/portrait-of-a-royal-bengal-tiger-alert-and-staring-at-the-camera-national-animal-of-bangladesh.jpg?s=612x612&w=0&k=20&c=0OCYv99Ktv3fJ-YYlg7SetHBJj3pIk58WY7GDy5VCtI=")
+// a.href = "https://www.aryanupadhyay.com"
+// console.dir(a);
+
+
+// let h1 = document.createElement("h1");
+// h1.textContent = "Hello Aryan";
+// document.body.appendChild(h1);
+
+// let h1 = document.querySelector("h1");
+// h1.remove();
+
+// let h1 = document.createElement("h1");
+// console.dir(h1);
+// h1.textContent = "Aryan Upadhyay";
+
+// document.querySelector("div").appendChild(h1);
+
+// let h1 = document.querySelector("h1");
+// h1.style.color = "red";
+// // h1.style.backgroundColor = "yellow";
+// h1.style.fontFamily = "Gilroy";
+// h1.style.textTransform = "capitalize"
+// console.dir(h1);
+
+// let h1 = document.querySelector("h1");
+// h1.classList.add("hulu");
+// h1.classList.remove("hulu");
+// h1.classList.toggle("hulu");
+
